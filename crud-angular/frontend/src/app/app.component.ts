1e9bdac8f3a1d4ceb050d1f01eb91a7065d22c9e
igor.cardoso <igor.cardoso@basis.com.br>
2021-08-13 10:13:47 -0300

    Inicio chart.js

--- crud-angular/frontend/src/app/app.component.ts
+++ crud-angular/frontend/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-root', // Seleor usado para esse componente
   templateUrl: 'app.component.html',
   styles: []
 })
-export class AppComponent { // Passa para o fronts
-  nome = 'Igor';
+export class AppComponent implements OnInit{ // Passa para o fronts
+  @ViewChild("meuCanvas", {static: true}) private elemento: ElementRef;
+  
+  constructor() { }
+
+  nome = 'Gráfico Legal';
+  ngOnInit() {
+    new Chart(this.elemento.nativeElement, {
+      type: 'line',
+      data: {
+        labels: ['1', '2', '3', '4', '5', '6'],
+        datasets: [
+          {
+            data: [1, 2, 3, 4, 5 ,6]
+          }
+        ]
+      }
+    });
+  }
 }
