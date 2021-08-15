d0827596f1431a2efc2ab2231186000888ca843c
ig-cardoso <iygorsilva@gmail.com>
2021-08-15 06:50:02 -0300

    Componentes

--- crud-angular/frontend/src/app/app.component.ts
+++ crud-angular/frontend/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root', // Seleor usado para esse componente
-  templateUrl: 'app.component.html',
+  templateUrl: 'app.component.html',  // Nome do template
   styles: []
 })
 export class AppComponent { // Passa para o fronts
