7f8b86dfa0d0e38c8b5ca90d4c30657d8d6c1a0d
ig-cardoso <iygorsilva@gmail.com>
2021-09-06 01:26:19 -0300

    Criaçao de diretiva, modificaçao na rota, etc

--- crud-angular/frontend/src/app/app-routing.module.ts
+++ crud-angular/frontend/src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
+/*
+  Nesse arquivo é tratado questões relacionado as rotas
+*/
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+// Importações
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+
+// Vai substituir esse componentes la no router-outlet que está no nav.component.html
+const routes: Routes = [
+	{
+		path: "",
+		component: HomeComponent
+	},
+	{
+		path: "produtos",  // Apenas o nome sem / no início
+		component: ProductCrudComponent
+	}
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- crud-angular/frontend/src/app/app.module.ts
+++ crud-angular/frontend/src/app/app.module.ts
@@ -11,16 +11,23 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component'; 
 
-
+// Importação do Mat
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    ProductCrudComponent,
+    RedDirective
   ],
   imports: [
     BrowserModule,
@@ -28,7 +35,8 @@ import { MatListModule } from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent] // Componente qeu será inicializado para carregar a aplicação
