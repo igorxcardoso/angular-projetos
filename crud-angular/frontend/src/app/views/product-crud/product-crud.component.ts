ec76990f51807ae14b886e36200dd223d0e3574e
igor.cardoso <igor.cardoso@basis.com.br>
2021-09-06 11:33:06 -0300

    Botao e redirect para URL /produtos/criar - Aula: Navegar para Tela de Criar Produtos

--- crud-angular/frontend/src/app/views/product-crud/product-crud.component.ts
+++ crud-angular/frontend/src/app/views/product-crud/product-crud.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-product-crud',
-  templateUrl: './product-crud.component.html',
-  styleUrls: ['./product-crud.component.css']
+	selector: 'app-product-crud',
+	templateUrl: './product-crud.component.html',
+	styleUrls: ['./product-crud.component.css']
 })
 export class ProductCrudComponent implements OnInit {
 
-  constructor() { }
+	// Vai injetar o trabuto do tipo Router automaticamente
+	constructor(private router: Router) { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
+	navigateToProductCreate(): void {
+		// Quando clicar no botão, vai navegar para um rota espcífica.
+		this.router.navigate(['/produtos/criar']);
+	}
+
+	// Teste
+	// atributoLegal = "qualquer";
+	// fazerAlgo(): void {
+	// 	console.log("Fazendo algo");
+	// }
 }
