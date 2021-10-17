1aa9c33bd0fd666e2cde91983b16e7a4e7d4b6ec
Igor Cardoso <iygorsilva@gmail.com>
2021-10-17 11:36:02 -0300

    Criacao de mensagens em caso do backend falhar

--- crud-angular/frontend/src/app/components/product/product.service.ts
+++ crud-angular/frontend/src/app/components/product/product.service.ts
@@ -1,3 +1,4 @@
+
 // Para expor o service para fora do módulo é preciso declarar em em providers no app.module
 
 import { HttpClient } from '@angular/common/http';
@@ -5,8 +6,8 @@ import { Product } from './product.model';   // Model (interface) criado
 
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar'
-import { Observable } from 'rxjs';
-
+import { EMPTY, Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 @Injectable({ // Esse decoretor indica que eu posso injetar essa clase em outras classes
 	providedIn: 'root' // Quando o service é provido a prtir do 'root' isso significa 
@@ -21,18 +22,24 @@ export class ProductService {
 
 	constructor(private snackBar: MatSnackBar,
 				private http: HttpClient) { }
-
-	showMessage(msg: string): void {
+							
+							// Padão de isError é flase
+	showMessage(msg: string, isError: boolean = false): void {
 		this.snackBar.open(msg, 'X', { // Esse 'X' é action que é indicação para fechar a notificação.
 			duration: 3000,
 			horizontalPosition: "right",
-			verticalPosition: "top"
+			verticalPosition: "top",
+			panelClass: isError ? ['msg-error'] : ['msg-sucess']	// Está definida no style global
 		});
 	}
 	
 	// Observable do tipo Product. Responsável por guardar os produtos. Requisição HTTP do tipo POST ao Backend.
 	create(produto: Product): Observable<Product> { 
-		return this.http.post<Product>(this.URL_POST, produto);
+		return this.http.post<Product>(this.URL_POST, produto).pipe(
+			map((obj) => obj),
+			catchError((myErro) => this.errorHandler(myErro))
+			
+		);
 	}
 
 	// Observable do tip0 lista de Product. Responsável por ler os produtos back. Requisição HTTP do tipo GET ao Backend.
@@ -48,12 +55,26 @@ export class ProductService {
 	// Observable do tipo Product. Responsável por atualizar os dados do banco. Requisição HTTP do tipo PUT ao Backend.
 	update(product: Product): Observable<Product> {
 		const url = `${this.URL_GET}/${product.id}`;
-		return this.http.put<Product>(url, product);
+		return this.http.put<Product>(url, product).pipe(
+			map((obj) => obj),
+			catchError((myErro) => this.errorHandler(myErro))
+
+		);
 	}
 
 	delete(id: any): Observable<Product>{
 		const url = `${this.URL_DELETE}/${id}`;
-		return this.http.delete<Product>(url);
+		return this.http.delete<Product>(url).pipe(
+			map((obj) => obj),
+			catchError((myErro) => this.errorHandler(myErro))
+
+		);
+	}
+
+	errorHandler(erro: any): Observable<any> {
+		console.log(erro);
+		this.showMessage("Ocorreu um erro!", true);
+		return EMPTY;
 	}
 
 	// <Tipo> -> Notação Generics
